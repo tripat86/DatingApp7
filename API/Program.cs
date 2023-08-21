cdf5287002dff2eac850901c250bb57ebd092230
Tripat <tripatsimransingh@gmail.com>
2023-08-21 08:55:30 +0530

    End of Section 3 - Building basic Angular CLient app with Https

--- API/Program.cs
+++ API/Program.cs
@@ -9,13 +9,12 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddCors();
 var app = builder.Build();
 
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+// Configure the HTTP request pipeline
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
